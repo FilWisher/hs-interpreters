642b236588de0f18dfc01c9061ce40d9def693f2
FilWisher <williamfisher@hotmail.co.uk>
2016-07-28 17:12:03 +0100

    adds lambda-calculus interpreter

---
+++ lambda.hs
@@ -0,0 +1,60 @@
+{-
+
+  Lazy lambda-calculus interpreter for evaluating
+  lambda-terms step by step
+
+-}
+
+module Interpreter where
+
+data Expr = Var Char
+          | Abs Char Expr
+          | App Expr Expr
+          | Sub Expr Expr Char
+          deriving (Show, Eq)
+ 
+stringify :: Expr -> String
+stringify (Var x) = [x]
+stringify (Abs h b) = "(\\" ++ [h] ++ "." ++ (stringify b) ++ ")"
+stringify (App m n) = stringify m ++ (stringify n)
+stringify (Sub e1 e2 c) = stringify e1 ++ "[" ++ (stringify e2) ++ "/" ++ [c] ++ "]"
+
+eval :: Expr -> Expr
+eval (Var x) = (Var x)
+eval (App m n) = case (eval m) of
+  (Abs x e) -> Sub e (eval n) x
+  e         -> App m (eval n)
+eval (Abs h b) = (Abs h b)
+eval (Sub e1 e2 c) = case (eval e1) of
+  (Var a)     -> if a == c then (eval e2) else (Var a)
+  (App e3 e4) -> App (Sub e3 e2 c) (Sub e4 e2 c)
+  (Abs h b)   -> Abs h (Sub b e2 c)
+
+
+evalFull x = do
+  putStrLn $ (stringify x) ++ "\t"  ++ (show x)
+  let y = eval x in 
+    if x == y
+      then putStrLn "terminated"
+      else evalFull y
+      
+    
+
+testVar = eval (Var 'c')
+
+tests =
+  [ ("testVar", testVar, Var 'c') 
+  --, ("testVar", testVar, Var 'c')
+  ]
+
+run = filter isJust $ 
+  map (\ (name, test, out) -> 
+    if test == out 
+      then Nothing 
+      else (Just name)) 
+  tests
+ 
+isJust :: Maybe a -> Bool
+isJust mb = case mb of
+  (Just k) -> True
+  otherwise -> False
