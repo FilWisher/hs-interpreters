4b4e543ae69799381dbc18f6c81332d185f6846f
FilWisher <williamfisher@hotmail.co.uk>
2016-08-03 09:57:51 +0100

    removes unused code

--- dcc.hs
+++ dcc.hs
@@ -8,8 +8,6 @@
     state through evaluation process.
 -}
 
--- DataKinds allows us to restrict over kind arguments
-
 module DCC where
 
 import Data.List
@@ -45,46 +43,10 @@ prettify (WithSubCont e1 e2) = mconcat ["withSubCont", prettify e1, prettify e2]
 prettify (PushSubCont e1 e2) = mconcat ["pushSubCont", prettify e1, prettify e2]
 prettify Hole = "_"
 prettify (Compose es) = foldr (++) "" $ intersperse ":" $ map prettify es
---
---data Expr (n :: ExprType) where
---  Var :: Char -> Expr Value
---  Abs :: Expr Value -> Expr n -> Expr Value
---  App :: Expr Value -> Expr n -> Expr Redex
---   
---  Sub :: Expr n -> Expr n -> Char -> Expr Redex
---
---  NewPrompt :: Expr Redex
---  PushPrompt :: Expr Prompt -> Expr n -> Expr Redex
---  WithSubCont :: Expr n -> Expr n -> Expr Redex
---  PushSubCont :: Expr n -> Expr n -> Expr Redex
---  
-  
-  --NewPrompt
-  --PushPrompt Expr Expr
-  --WithSubCont Expr Expr
-  --PushSubCont Expr Expr
-  --Hole
-  --Prompt Int
-  --Compose [Expr] [Expr]  -- for composing frames
-
---cool = [] :: List (Expr ExprType)
---deriving instance Eq (Expr n)
 
 data State = State Expr Expr [Frame] Expr
   deriving (Show, Eq)
   
---splitBefore :: [Expr] -> Expr -> [Expr]
---splitBefore (e:es) p
---  | p /= e    = e:(splitBefore es p)
---  | otherwise = [e]
---splitBefore [] p = []
---
---splitAfter :: [Expr] -> Expr -> [Expr]
---splitAfter (e:es) p
---  | p /= e = splitAfter es p
---  | otherwise = es
---splitAfter [] p = []
-
 promptMatch :: Expr -> (Frame -> Bool)
 promptMatch p = case p of
   P p -> \q -> case q of
@@ -142,12 +104,6 @@ eval (State (Sub (PushSubCont e1 e2) y x) d es q) = State (PushSubCont (Sub e1 y
 
 -- TODO: this is unbelievably hacky
 -- returning a value
-
---eval (State (Val v) d es q) = State (App (Val v) d) Hole es q
---eval (State (Val v) Hole es q) = case length es of
---  0 -> State (Val v) Hole es q
---  otherwise -> State (Val v) (extractFrame $ head es) q
-
 eval (State (Var x) d es q) = evalVal $ State (Var x) d es q
 eval (State (Abs x m) d es q) = evalVal $ State (Abs x m) d es q
 eval (State (P p) d es q) = evalVal $ State (P p) d es q
@@ -170,64 +126,3 @@ evalFull s = do
     if s == s'
       then putStrLn "terminated"
       else evalFull s'
---eval (State (Var x) d es q) = State (App (Var x) d) Hole es q
---eval (State (Abs x m) d es q) = State (App (Abs x m) d) Hole es q
---eval (State v d (e:es) q) = case d of
---  Hole -> (State (App v d) Hole es q)
---  otherwise -> case e of
---    Prompt _  -> (State v Hole es q) 
---    otherwise -> (State v e es q) 
-   
-
-
---data Expr :: * -> * where
---  Var :: Char -> Expr Char
---  Abs :: Char -> Expr a -> Expr a
---  App :: Expr Char -> Expr a -> Expr a
---  Sub :: Expr a -> Expr a -> Char -> Expr a
---  
---  P :: Int -> Expr Int
---  NewPrompt :: Expr Int
---  PushPrompt :: Expr Int -> Expr a -> Expr a
---  WithSubCont :: Expr a -> Expr a -> Expr a
---  PushSubCont :: Expr a -> Expr a -> Expr a
---  Hole :: Expr Empty
---
---deriving instance Show (Expr a)
---deriving instance Eq (Expr a)
---
---data State a  = State (Expr a) (Expr a) [(Expr a)] (Expr Int)
---  deriving (Show, Eq)
---
---eval :: State a -> State a
---eval x = x
-
--- returning a value
---eval (State v d (e:es) q) = (State (App v d) e es q)
-
---data State = State (Expr a) Int [(Int, (Expr a))]
---  deriving (Show, Eq)
-
---data State a = State (Expr a) (Expr Int) [Expr a] (Expr Int)
---  deriving (Show, Eq)
-
---show(Expr a) :: (Expr a) -> String
---show(Expr a) (Var x) = [x]
---show(Expr a) (Abs h b) = "(\\" ++ [h] ++ "." ++ (show(Expr a) b) ++ ")"
---show(Expr a) (App m n) = show(Expr a) m ++ (show(Expr a) n)
---show(Expr a) (Sub e1 e2 c) = show(Expr a) e1 ++ "[" ++ (show(Expr a) e2) ++ "/" ++ [c] ++ "]"
---
---show(Expr a) (NewPrompt) = "newPrompt"
---show(Expr a) (PushPrompt p exp) = "pushPrompt " ++ (show p) ++ (show exp)
---show(Expr a)  (P i) = "p(" ++ (show i) ++ ")"
---
---showState :: State -> String
---showState (State expr q e) = "(" ++ (show(Expr a) expr) ++ ", " ++ (show q) ++ ", " ++ (show e) ++ ")"
---
---eval :: State -> State
---eval (State (Var a) q e)            = (State (Var a) q e)
---eval (State NewPrompt q e)          = (State (P q) (q+1) e)
---eval (State (PushPrompt p ex) q  e) = (State ex q ((p, ex):e))
---
---test(Expr a) expr = showState $ eval (State expr 0 [])
---test expr = showState $ eval expr
